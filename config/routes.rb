acab4a36d98f251b549f69b04ca612a79d73949e
rika kaminoyama <richun.06@gmail.com>
2023-08-15 20:35:32 +0900

    ログイン機能の実装、ヘッダーのリンクの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
