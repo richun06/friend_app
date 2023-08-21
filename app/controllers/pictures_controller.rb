a5b3112c46e8224b075dc36c355d9d76db413161
rika kaminoyama <richun.06@gmail.com>
2023-08-21 23:40:00 +0900

    詳細機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,6 +19,10 @@ class PicturesController < ApplicationController
     # byebug
   end
 
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def edit
     @picture = Picture.find(params[:id])
   end
